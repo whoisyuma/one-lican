593741344652a85eb691c0e7b61a29dc449e69ea
whoisyuma <whoisyuma.0913@gmail.com>
2025-10-07 15:40:51 +0900

    add pending

---
+++ app/group/[id]/LoadingLink.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import { useRouter } from "next/navigation";
+import React, { useTransition } from "react";
+
+interface LoadingLinkProps {
+    href: string;
+    children: React.ReactNode;
+    className: string;
+}
+
+export default function LoadingLink({ href, children, className }: LoadingLinkProps) {
+    const router = useRouter()
+    const [isPending, startTransition] = useTransition();
+
+    const handleClick = () => {
+        startTransition(() => {
+            router.push(href);
+        });
+    };
+
+    return (
+        <button onClick={handleClick} disabled={isPending} className={className}>
+            {isPending ? '読み込み中...' : children}
+        </button>
+    )
+}
--- app/group/[id]/page.tsx
+++ app/group/[id]/page.tsx
@@ -1,5 +1,5 @@
 import { createClient } from "@/utils/supabase/server";
-import Link from "next/link";
+import LoadingLink from "./LoadingLink";
 
 interface PageProps {
     params: Promise<{id: string}>;
@@ -19,7 +19,7 @@ export default async function GroupPage({ params }: PageProps) {
     const group = groupData;
     
     return (
-        <div className="bg-gray-100 min-h-screen">
+        <div className="bg-gray-200 min-h-screen">
             <div className="lg:w-1/2 md:w-2/3 w-full m-auto px-5 md:px-0 pt-10">
                 <div className="flex flex-col items-center mb-10">
                     <img src="/check.png" alt="チェックマーク" className="md:w-25 md:h-25 w-20 h-20 mb-15"/>
@@ -32,9 +32,9 @@ export default async function GroupPage({ params }: PageProps) {
                 </div>
 
                 <div className="flex justify-center">
-                    <Link href={`/group/${group?.id}/home`} className="bg-sky-600 text-center text-white rounded-3xl md:text-xl text-base md:py-3 py-2 w-2/3">
+                    <LoadingLink href={`/group/${group?.id}/home`} className="bg-sky-600 text-center text-white rounded-3xl md:text-xl text-base md:py-3 py-2 w-2/3">
                         グループページへ進む
-                    </Link>
+                    </LoadingLink>
                 </div>
             </div>
         </div>
