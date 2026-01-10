c6c8007274c5315c608cf7692e1c5258b95246b0
Yuma <whoisyuma.0913@gmail.com>
2026-01-10 14:23:24 +0900

    add loading page

---
+++ app/group/[id]/payments/[payment_id]/edit/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-200">
+      <p className="text-sky-700 text-sm md:text-base font-semibold tracking-wide animate-pulse">
+        読み込み中…
+      </p>
+    </div>
+  );
+}
